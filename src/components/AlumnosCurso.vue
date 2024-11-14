51e1e443d2a39d36a8abbc190e1a6f4b90a15c19
manu12perez <manu12perez@gmail.com>
2024-11-14 17:20:25 +0100

    aasa

--- src/components/AlumnosCurso.vue
+++ src/components/AlumnosCurso.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container mt-4">
         <router-link to="/" class="btn btn-secondary mb-3"> Volver </router-link>
         <h1>Alumnos del curso</h1>
     </div>
